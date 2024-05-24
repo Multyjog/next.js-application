48c56c8a52664d68d291f47d0fdb8559b271b57f
Multyjog <simon.kotov.web@gmail.com>
2024-05-24 16:03:22 +0700

    Loading UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html data-theme="retro" lang="en">
       <body className={inter.className}>
         <NavBar />
-        <main className="p-5">{children}</main>
+        <main className="p-5">
+          <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
+        </main>
       </body>
     </html>
   );
