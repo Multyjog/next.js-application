f56cfb8c8ffe012fecb4451785b51c27a5b1c293
Multyjog <simon.kotov.web@gmail.com>
2024-05-24 15:56:19 +0700

    Programmatic navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+  const router = useRouter();
+  return (
+    <button onClick={() => router.push("/users")} className="btn btm-primary">
+      Create
+    </button>
+  );
 };
 
 export default NewUserPage;
