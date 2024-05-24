b4c9d725328b7629c5ed4facbc619abe32afaca7
Multyjog <simon.kotov.web@gmail.com>
2024-05-24 15:51:25 +0700

    Layout system Added Navbar to all pages Added special layout for admin section

---
+++ app/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="flex bg-slate-200 p-5">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users" className="mr-5">
+        Users
+      </Link>
+      <Link href="/admin" className="mr-5">
+        Admin
+      </Link>
+    </div>
+  );
+};
+
+export default NavBar;
