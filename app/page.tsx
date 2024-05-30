2e443d76048b41fd3d57b511f3a7cd2e0a06baaa
Multyjog <simon.kotov.web@gmail.com>
2024-05-30 16:20:33 +0700

    Responsive images And Next.js's way of compression and working with Image tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
-import Link from "next/link";
 import Image from "next/image";
-import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import aprilia from "@/public/images/aprilia.jpg";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      {/* <Image src={aprilia} alt="Group of biker on aprilia" /> */}
+      <Image
+        src="https://picsum.photos/1920/1080"
+        fill
+        alt="Group of biker on aprilia"
+        className="object-cover"
+        // Full cover
+        // sizes="100vw"
+
+        // (@media property)
+        sizes="(max-width: 480px) 100vw, (max-width:768px) 50vw, 33vw "
+        quality={100}
+        // Lazy by default, if I need image to download before user will see it, I set PRIORITY props
+        priority
+      />
     </main>
   );
 }
