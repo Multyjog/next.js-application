a0bd74db31c871e2db66cbcd7d3f467f439c0871
Multyjog <simon.kotov.web@gmail.com>
2024-06-03 18:49:20 +0700

    Fixed buidling errors

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -7,7 +7,7 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: params.id },
   });
   if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
@@ -24,7 +24,7 @@ export async function PUT(
     return NextResponse.json(validationResult.error?.errors, { status: 400 });
 
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: params.id },
   });
 
   if (!user)
@@ -42,7 +42,7 @@ export async function DELETE(
   { params }: { params: { id: string } }
 ) {
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: params.id },
   });
 
   if (!user)
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
 "use client";
-
-import { useState } from "react";
 // import { getServerSession } from "next-auth";
 // import { authOptions } from "./api/auth/[...nextauth]/route";
 
-// Dynamic import
-import dynamic from "next/dynamic";
-const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
-  ssr: false, // Disable pre-rendering
-  loading: () => <p>Loading...</p>,
-});
-
 export default function Home() {
   // const session = await getServerSession(authOptions);
-  const [isVisible, setVisible] = useState(false);
   return (
     <main className="relative h-screen">
       <h1>Hello world</h1>
-      <button onClick={() => setVisible(true)}>Show</button>
-      {isVisible && <HeavyComponent />}
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [{ name: "c" }, { name: "a" }, { name: "b" }];
+          const sortedArr = _.orderBy(users, ["name"]);
+          console.log(sortedArr);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
 }
